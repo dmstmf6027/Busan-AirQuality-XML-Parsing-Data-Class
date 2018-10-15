3a984c4563a81fea479c10c1aa0717ef4e68ac10
201631053 <hubot@githhub@com>
2018-10-15 14:32:16 +0900

    Initial Commit

---
+++ Busan-AirQuality-XML-Parsing-Data-Class/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Busan-AirQuality-XML-Parsing-Data-Class
+//
+//  Created by D7703_04 on 2018. 10. 15..
+//  Copyright © 2018년 D7703_04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
